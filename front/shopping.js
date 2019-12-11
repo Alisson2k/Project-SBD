917dcb83366c2747c25bc79d2c97a8d78432c403
alissongp <alisson.paula@neppoti.com.br>
2019-12-11 18:13:18 -0300

    edit shopping js

--- front/shopping.js
+++ front/shopping.js
@@ -1,43 +1,65 @@
 getProducts();
 
 function getProducts() {
-    for (var i = 0; i < 6; i++) {
-        var putHere = document.getElementById("putHere");
-        var img = document.createElement("img");
-        img.src = "./img/P-IMAGE.png";
+    var listProducts = document.getElementById("listProducts");
+    for (var i = 0; i < 10; i++) {
+        var productDOM = [
+            document.createElement("div"),  // 0
+            document.createElement("div"),  // 1
+            document.createElement("div"),  // 2
+            document.createElement("a"),    // 3
+            document.createElement("img"),  // 4
+            document.createElement("div"),  // 5
+            document.createElement("div"),  // 6
+            document.createElement("div"),  // 7
+            document.createElement("h4"),   // 8
+            document.createElement("div"),  // 9
+            document.createElement("div"),  // 10
+            document.createElement("div"),  // 11
+            document.createElement("p"),    // 12
+        ]
 
-        var div1 = document.createElement("div");
-        div1.className = "product-img";
-        div1.appendChild(img);
+        productDOM[4].src = "./img/P-IMAGE.png"                // Imagem do produto
+        productDOM[4].alt = "Titulo do produto"                // Titulo do produto
 
-        var div2 = document.createElement("div");
-        div2.className = "single-product mb-35";
-        div2.appendChild(div1);
+        productDOM[3].href = ""                                // href para o produto
+        productDOM[3].appendChild(productDOM[4]);
 
-        var div3 = document.createElement("div");
-        div3.className = "custom-col-5 custom-col-style";
-        div3.appendChild(div2);
+        productDOM[2].className = "product-img";
+        productDOM[2].appendChild(productDOM[3]);
+
+        productDOM[8].textContent = "Titulo do produto"        // Titulo do produto
         
-        putHere.appendChild(div3);
-    }
-}
+        productDOM[7].className = "product-title";
+        productDOM[7].appendChild(productDOM[8]);
+        
+        productDOM[9].className = "product-price";
+        productDOM[9].textContent = "Preço"                     // Preço do produto
 
-{/* <div class="custom-col-5 custom-col-style">
-    <div class="single-product mb-35">
-        <div class="product-img">
-            <a href="">
-                <img src="./img/P-IMAGE.png" alt="">
-            </a>
-        </div>
-        <div class="product-content">
-            <div class="product-title-price">
-                <div class="product-title"><h4>Product title</h4>
-            </div>
-            <div class="product-price">100,00</div>
-        </div>
-            <div class="product-cart-categori">
-                <div class="product-cart"><p>Some quick example text to build on the card title and make up the bulk of the card's content.</p></div>
-            </div>
-        </div>
-    </div>
-</div> */}
+        productDOM[6].className = "product-title-price";
+        productDOM[6].appendChild(productDOM[7]);
+        productDOM[6].appendChild(productDOM[9]);
+
+        productDOM[12].textContent = "Descrição do produto"     // Descrição do produto
+        
+        productDOM[11].className = "product-cart;"
+        productDOM[11].appendChild(productDOM[12]);
+        
+        productDOM[10].className = "product-cart-categori";
+        productDOM[10].appendChild(productDOM[11]);
+        
+        productDOM[5].className = "product-content";
+        productDOM[5].appendChild(productDOM[6]);
+        productDOM[5].appendChild(productDOM[10]);
+
+        productDOM[1].className = "single-product mb-35";
+        productDOM[1].appendChild(productDOM[2]);
+        productDOM[1].appendChild(productDOM[5]);
+
+        productDOM[0].className = "custom-col-5 custom-col-style";
+        productDOM[0].appendChild(productDOM[1]);
+
+        listProducts.appendChild(productDOM[0]);
+    }
+
+}
